a2657331603a53d84913670dd29f770651226682
David Lumaye <dlu@monizze.be>
2020-07-08 09:23:33 +0200

    Release v1.0.0

---
+++ src/Collection.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace Chindit\Collection;
+
+class Collection implements \Iterator
+{
+    private array $data;
+    private \ArrayIterator $iterator;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+        // Prepare iterator
+        $this->iterator = new \ArrayIterator($this->data);
+    }
+
+    public function all(): array
+    {
+        return array_values($this->data);
+    }
+
+    public function contains($search): bool
+    {
+        foreach ($this->data as $item) {
+            if ($item === $search) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function current()
+    {
+        return $this->iterator->current();
+    }
+
+    public function each($callback): self
+    {
+        if (!is_callable($callback)) {
+            return $this;
+        }
+
+        foreach ($this->data as $datum) {
+            if ($callback($datum) === false) {
+                break;
+            }
+        }
+
+        return $this;
+    }
+
+    public function filter($callback): self
+    {
+        if (!is_callable($callback)) {
+            return $this;
+        }
+
+        $accepted = new self();
+
+        foreach ($this->data as $datum) {
+            if ($callback($datum) === true) {
+                $accepted->push($datum);
+            }
+        }
+
+        return $accepted;
+    }
+
+    public function first()
+    {
+        return count($this->data) > 0 ? $this->data[0] : null;
+    }
+
+    public function flatten(int $depth = 500): self
+    {
+        $result = [];
+
+        foreach ($this->data as $item) {
+            $item = $item instanceof Collection ? $item->all() : $item;
+
+            if (!is_array($item)) {
+                $result[] = $item;
+            } elseif ($depth === 1) {
+                $result = array_merge($result, array_values($item));
+            } else {
+                $result = array_merge($result, (new self($item))->flatten($depth - 1)->toArray());
+            }
+        }
+
+        return new self($result);
+    }
+
+    public function get($key, $defaultValue = null)
+    {
+        return $this->has($key) ? $this->data[$key] : $defaultValue;
+    }
+
+    public function has($key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return count($this->data) === 0;
+    }
+
+    public function isNotEmpty(): bool
+    {
+        return !$this->isEmpty();
+    }
+
+    public function key()
+    {
+        return $this->iterator->key();
+    }
+
+    public function keyBy($callback): self
+    {
+        $results = [];
+        foreach ($this->data as $item) {
+            $results[] = (is_callable($callback)) ? $callback($item) : $this->getValueByAccessor($item, $callback);
+        }
+
+        return new self(array_combine($results, $this->data));
+    }
+
+    public function keys(): self
+    {
+        return new self(array_keys($this->data));
+    }
+
+    public function map($callback): self
+    {
+        if (!is_callable($callback)) {
+            return $this;
+        }
+
+        $result = [];
+
+        foreach ($this->data as $item) {
+            $result[] = $callback($item);
+        }
+
+        return new self($result);
+    }
+
+    public function merge(self $collection): self
+    {
+        return new self(array_merge($this->data, $collection->toArray()));
+    }
+
+    public function mergeRecursive(self $collection): self
+    {
+        return new self(array_merge_recursive($this->data, $collection->toArray()));
+    }
+
+    public function next(): void
+    {
+        $this->iterator->next();
+    }
+
+    public function pluck(string $name): self
+    {
+        if (empty($this->data)) {
+            return new self();
+        }
+
+        $results = new self();
+        foreach ($this->data as $item) {
+            $results->push($this->getValueByAccessor($item, $name));
+        }
+
+        return $results->filter(fn($item) => $item !== null);
+    }
+
+    public function push($item): self
+    {
+        $this->data[] = $item;
+
+        return $this;
+    }
+
+    public function rewind(): void
+    {
+        $this->iterator->rewind();
+    }
+
+    public function toArray(): array
+    {
+        return array_map(function ($value) {
+            return $value instanceof self ? $value->toArray() : $value;
+        }, $this->data);
+    }
+
+    public function unique(): self
+    {
+        return new self(array_unique($this->data));
+    }
+
+    public function valid(): bool
+    {
+        return $this->iterator->valid();
+    }
+
+    private function getValueByAccessor($item, $name)
+    {
+        if (is_array($item)) {
+            if (isset($item[$name])) {
+                return $item[$name];
+            }
+        } elseif (is_object($item)) {
+            if (method_exists($item, $name)) {
+                return $item->$name();
+            } elseif (method_exists($item, 'get' . ucfirst($name))) {
+                $methodName = 'get' . ucfirst($name);
+                return $item->$methodName();
+            } elseif (property_exists($item, $name)) {
+                return $item->$name;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/CollectionTest.php
@@ -0,0 +1,352 @@
+<?php
+
+namespace Chindit\Collection\Tests;
+
+use Chindit\Collection\Collection;
+use PHPUnit\Framework\TestCase;
+
+class CollectionTest extends TestCase
+{
+    public function testToArray(): void
+    {
+        $source = [
+            'a',
+            'b' => [
+                'c' => 'd',
+            ],
+        ];
+
+        $collection = new Collection($source);
+
+        $this->assertEquals($source, $collection->toArray());
+    }
+
+    public function testPluckWithEmptyData(): void
+    {
+        $this->assertEquals(new Collection(), (new Collection())->pluck('name'));
+    }
+
+    public function testPluckWithArray(): void
+    {
+        $source = [
+            'a',
+            'b' => [
+                'name' => 'd',
+            ],
+        ];
+
+        $collection = new Collection($source);
+
+        $this->assertEquals(['d'], $collection->pluck('name')->toArray());
+    }
+
+    public function testPluckWithPublicMethod(): void
+    {
+        $testObjectClass = new class {
+            private $name = 'chindit';
+            public function name(): string
+            {
+                return $this->name;
+            }
+        };
+        $testObject = new $testObjectClass();
+
+        $collection = new Collection([
+            'a',
+            'b' => [
+                'name' => 'd',
+            ],
+            'd' => $testObject
+        ]);
+
+        $this->assertEquals(['d', 'chindit'], $collection->pluck('name')->toArray());
+    }
+
+    public function testPluckWithPublicGetter(): void
+    {
+        $testObjectClass = new class {
+            private $name = 'chindit';
+            public function getName(): string
+            {
+                return $this->name;
+            }
+        };
+        $testObject = new $testObjectClass();
+
+        $collection = new Collection([
+            'a',
+            'b' => [
+                'name' => 'd',
+            ],
+            'd' => $testObject
+        ]);
+
+        $this->assertEquals(['d', 'chindit'], $collection->pluck('name')->toArray());
+    }
+
+    public function testPluckWithPublicAttribute(): void
+    {
+        $testObjectClass = new class {
+            public $name = 'chindit';
+        };
+        $testObject = new $testObjectClass();
+
+        $collection = new Collection([
+            'a',
+            'b' => [
+                'name' => 'd',
+            ],
+            'd' => $testObject
+        ]);
+
+        $this->assertEquals(['d', 'chindit'], $collection->pluck('name')->toArray());
+    }
+
+    public function testPush(): void
+    {
+        $collection = new Collection(['apple', 'pear']);
+
+        $collection->push('orange');
+
+        $this->assertEquals(['apple', 'pear', 'orange'], $collection->toArray());
+    }
+
+    public function testContainsWithNotFoundData(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertFalse($collection->contains('banana'));
+    }
+
+    public function testContainsWithValidData(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertTrue($collection->contains('orange'));
+    }
+
+    public function testMapWithInvalidParam(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals($collection, $collection->map('yeah'));
+    }
+
+    public function testMapWithCallback(): void
+    {
+        $sourceCollection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals(['applee', 'peare', 'orangee'], $sourceCollection->map(function($item)
+        {
+            return $item . 'e';
+        })
+        ->toArray());
+    }
+
+    public function testFirstWithNoData(): void
+    {
+        $this->assertNull((new Collection())->first());
+    }
+
+    public function testFirst(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals('apple', $collection->first());
+    }
+
+    public function testFilterWithNotCallableMethod(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals($collection, $collection->filter(null));
+    }
+
+    public function testFilterWithNonTrueMethod(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals([], $collection->filter(fn(string $item) => $item)->toArray());
+    }
+
+    public function testFilterWithValidCallback(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals(
+            ['apple', 'pear'],
+            $collection->filter(fn(string $item) => strpos($item, 'p') !== false)->toArray()
+        );
+    }
+
+    public function testHas(): void
+    {
+        $collection = new Collection(['a' => 'apple', 'p' => 'pear', 'o' => 'orange']);
+
+        $this->assertFalse($collection->has('apple'));
+        $this->assertTrue($collection->has('a'));
+        $this->assertFalse($collection->has(0));
+    }
+
+    public function testGet(): void
+    {
+        $collection = new Collection(['a' => 'apple', 'p' => 'pear', 'o' => 'orange']);
+
+        $this->assertEquals('apple', $collection->get('a'));
+        $this->assertNull($collection->get('banana'));
+        $this->assertEquals('apple', $collection->get('banana', 'apple'));
+    }
+
+    public function testKeys(): void
+    {
+        $collection = new Collection(['a' => 'apple', 'p' => 'pear', 'o' => 'orange']);
+
+        $this->assertEquals(['a', 'p', 'o'], $collection->keys()->toArray());
+    }
+
+    public function testKeysWithStandardArray(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals([0, 1, 2], $collection->keys()->toArray());
+    }
+
+    public function testEmptyIsTrue(): void
+    {
+        $collection = new Collection();
+
+        $this->assertTrue($collection->isEmpty());
+        $this->assertFalse($collection->isNotEmpty());
+    }
+
+    public function testMerge(): void
+    {
+        $first = new Collection(['apple', 'pear']);
+        $second = new Collection(['orange']);
+
+        $this->assertEquals(['apple', 'pear', 'orange'], $first->merge($second)->toArray());
+        $this->assertEquals(['orange', 'apple', 'pear'], $second->merge($first)->toArray());
+    }
+
+    public function testMergeWithSubArrayAndNonRecursive(): void
+    {
+        $first = new Collection(['a' => ['fruits' => ['apple', 'ananas'], 'vegetables' => ['artichoke', 'aubergine']]]);
+        $second = new Collection(['a' => ['vegetables' => ['asparagus']]]);
+
+        // $second will overwrite $first
+        $this->assertEquals($second->toArray(), $first->merge($second)->toArray());
+    }
+
+    public function testMergeRecursive(): void
+    {
+        $first = new Collection(['a' => ['fruits' => ['apple', 'ananas'], 'vegetables' => ['artichoke', 'aubergine']]]);
+        $second = new Collection(['a' => ['vegetables' => ['asparagus']]]);
+
+        $this->assertEquals(
+            ['a' => ['fruits' => ['apple', 'ananas'], 'vegetables' => ['artichoke', 'aubergine', 'asparagus']]],
+            $first->mergeRecursive($second)->toArray()
+        );
+    }
+
+    public function testIsEmptyIsFalse(): void
+    {
+        $collection = new Collection(['a']);
+
+        $this->assertFalse($collection->isEmpty());
+        $this->assertTrue($collection->isNotEmpty());
+    }
+
+    public function testFlattenWithSingleLevelArray(): void
+    {
+        $collection = new Collection(['apple', 'pear', 'orange']);
+
+        $this->assertEquals($collection, $collection->flatten());
+    }
+
+    public function testFlattenWithMultipleLevelArray(): void
+    {
+        $collection = new Collection(
+            ['apple',
+             'fruits' => [
+                 'banana',
+                'exotics' => [
+                    'coco',
+                    'mango',
+                    'very_exotic' => [
+                        'lychee',
+                        'durian',
+                    ],
+                 ],
+             ],
+             'orange'
+            ]
+        );
+
+        $expectedCollection = new Collection(['apple', 'banana', 'coco', 'mango', 'lychee', 'durian', 'orange']);
+
+        $this->assertEquals($expectedCollection, $collection->flatten());
+    }
+
+    public function testFlattenWithLimitedDepth(): void
+    {
+        $collection = new Collection(
+            ['apple',
+             'fruits' => [
+                 'banana',
+                 'exotics' => [
+                     'coco',
+                     'mango',
+                     'very_exotic' => [
+                         'lychee',
+                         'durian',
+                     ],
+                 ],
+             ],
+             'orange'
+            ]
+        );
+
+        $expectedCollection = new Collection(['apple', 'banana', 'coco', 'mango', ['lychee', 'durian'], 'orange']);
+
+        $this->assertEquals($expectedCollection, $collection->flatten(2));
+    }
+
+    public function testAllWithEmptyDataSet(): void
+    {
+        $this->assertEmpty((new Collection())->all());
+    }
+
+    public function testAll(): void
+    {
+        $collection = new Collection(
+            [
+                'a' => 1,
+                'b' => 2,
+                'c' => 3,
+                'd' => ['e' => 4]
+            ]
+        );
+
+        $this->assertEquals([1, 2, 3, ['e' => 4]], $collection->all());
+    }
+
+    public function testIterator(): void
+    {
+        $collection = new Collection(
+            [
+                'a' => 1,
+                'b' => 2,
+                'c' => 3,
+                'd' => ['e' => 4]
+            ]
+        );
+
+        $this->assertEquals(1, $collection->current());
+        $collection->next();
+        $this->assertEquals(2, $collection->current());
+        $this->assertEquals('b', $collection->key());
+        $collection->rewind();
+        $this->assertEquals('a', $collection->key());
+        $this->assertTrue($collection->valid());
+    }
+}
